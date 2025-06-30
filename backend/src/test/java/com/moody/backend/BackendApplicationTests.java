7b9313246e7ab0c586c866364bc1dbaeced3e5cb
sw801733 <sw801733@naver.com>
2025-06-30 23:21:23 +0900

    init: backend 초기 세팅

---
+++ backend/src/test/java/com/moody/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moody.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
